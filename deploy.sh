a93f9aa3dea4e98ba89c55d4d30e17ad974316f3
Kent Sommer <kent.sommer13@gmail.com>
2016-10-26 16:57:08 +0900

    test

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 msg="Update Blog Content"
+
 if [ $# -eq 1 ]
   then msg="$1"
 fi
-git add * && git commit -m "$msg" && git push origin master
+
+git add -A && git commit -m "$msg" && git push origin master
 hugo
 cd public/
-git add * && git commit -m "$msg" && git push origin master
+git add -A && git commit -m "$msg" && git push origin master
 cd ../
